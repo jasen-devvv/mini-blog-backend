bf3e7ee451c8e8a629debdc6c2b94c4352a414cb
jasen-devvv <janadevofficial@gmail.com>
2025-02-28 17:53:41 +0800

    feat(db): configure PostgreSQL connection with GORM

---
+++ config/database.go
@@ -0,0 +1,38 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/jasen-devvv/mini-blog-backend/models"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDatabase() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error laoding .env file")
+	}
+
+	dbHost := os.Getenv("DB_HOST")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	dbPort := os.Getenv("DB_PORT")
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=require TimeZone=UTC", dbHost, dbUser, dbPassword, dbName, dbPort)
+
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database")
+	}
+
+	DB.AutoMigrate(&models.User{}, &models.Article{}, &models.Comment{})
+
+	fmt.Println("Database connected successfully")
+}
